f89ffc5b2f465e5d85192af347f535916e398c35
Arasuvel Theerthapathy <arasuvel@D005.local>
2016-11-04 13:34:27 +0530

    Sample Local Notification Example implementation.

--- LocalNotificationExampleiOS10/ViewControllers/ViewController.m
+++ LocalNotificationExampleiOS10/ViewControllers/ViewController.m
@@ -7,12 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <UserNotifications/UserNotifications.h>
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+
+#pragma mark - View Life Cycle Methods
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,6 +27,60 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Action Methods
+
+- (IBAction)sampleButtonPressed:(id)sender {
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    
+    [center getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+        if (settings.authorizationStatus == UNAuthorizationStatusAuthorized) {
+            // Notifications allowed
+             [self scheduleLocalNotifications];
+        } else {
+           // Notifications not allowed
+        }
+    }];
+}
+
+#pragma mark - Custom Methods
+
+
+- (void)scheduleLocalNotifications {
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    
+    UNNotificationAction *snoozeAction = [UNNotificationAction actionWithIdentifier:@"Snooze"
+                                                                              title:@"Snooze" options:UNNotificationActionOptionNone];
+    UNNotificationAction *deleteAction = [UNNotificationAction actionWithIdentifier:@"Delete"
+                                                                              title:@"Delete" options:UNNotificationActionOptionDestructive];
+    
+    UNNotificationCategory *category = [UNNotificationCategory categoryWithIdentifier:@"UYLReminderCategory"
+                                                                              actions:@[snoozeAction,deleteAction] intentIdentifiers:@[]
+                                                                              options:UNNotificationCategoryOptionNone];
+    NSSet *categories = [NSSet setWithObject:category];
+    
+    [center setNotificationCategories:categories];
+    
+    
+    UNMutableNotificationContent *content = [UNMutableNotificationContent new];
+    content.title = @"Don't forget";
+    content.body = @"Buy some milk";
+    content.categoryIdentifier = @"UYLReminderCategory";
+    content.sound = [UNNotificationSound defaultSound];
+    
+    UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:5 repeats:NO];
+    
+    NSString *identifier = @"UYLLocalNotification";
+    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger];
+    
+    [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+        if (error != nil) {
+            NSLog(@"Something went wrong: %@",error);
+        }
+    }];
+    
+    
 }
 
 
